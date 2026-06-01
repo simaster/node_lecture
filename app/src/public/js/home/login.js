a992e46f58d690fdc88b84172b7da29d48fbd8dd
simaster <simaster86@naver.com>
2026-06-01 11:11:19 +0900

    dom 을 이용한 객체 제어하기

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,5 +1,32 @@
 "use strict";
 
-console.log("login.js loaded");
-console.log("login.js loaded successfully");
-    
+// console.log("login.js loaded");
+// console.log("login.js loaded successfully");
+
+const id = document.querySelector("#id"); // id input 요소 선택
+    // pw = document.querySelector("#pw"); // pw input 요소 선택
+    // loginBtn = document.querySelector("#login-btn"); // login 버튼 요소 선택
+
+const pw = document.querySelector("#pw"); // pw input 요소 선택
+const loginBtn = document.querySelector("#login-btn"); // login 버튼 요소 선택
+
+loginBtn.addEventListener("click", login);
+function login() {  
+  const req = {
+    id: id.value,
+    pw: pw.value,
+  };
+  console.log("Login request:", req);
+}
+
+
+
+// const loginBtn = document.querySelector("#login-btn"); // login 버튼 요소 선택
+// loginBtn.addEventListener("click", function() {
+//     console.log("Login button clicked");}
+// );
+
+// console.log("id:", id);
+// console.log(id);
+// console.log("pw:", pw);
+// console.log("loginBtn:", loginBtn);
